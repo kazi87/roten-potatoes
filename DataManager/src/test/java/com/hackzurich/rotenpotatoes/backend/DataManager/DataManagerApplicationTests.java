a8e0e3b733bca1b9819833cf5dc72708fded9d45
kazi <xk.zielinski@gmail.com>
2017-09-16 14:24:17 +0200

    Init commit for DataManager

---
+++ DataManager/src/test/java/com/hackzurich/rotenpotatoes/backend/DataManager/DataManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hackzurich.rotenpotatoes.backend.DataManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
